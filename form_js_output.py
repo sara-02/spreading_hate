a5958b39c3fbe28641c888617889868eead98336
sara-02 <sarahmasud02@gmail.com>
2020-05-27 20:00:38 +0530

    fix iteration zero statusga *.py

--- form_js_output.py
+++ form_js_output.py
@@ -10,11 +10,12 @@ class FormOutput():
     def update_output_scores(self, iteration_results):
         format_final = []
         tobe_hate_update = set()
-        for each_result in iteration_results[1:]:
+        for each_result in iteration_results:
             return_scores = deepcopy(self.user_data)
             status = each_result.get('status', {})
             for each_user in return_scores:
-                if each_user in status:
+                if each_user in status and status[each_user]==1:
+                    print(each_user, each_result['iteration'])
                     return_scores[each_user]['score'] = 1
                     tobe_hate_update.add(each_user)
                 else:
